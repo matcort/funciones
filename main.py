309849bd156186a7dc3ae165e36bf74aa7c348d8
Matias Cortes <mat.cort@gmail.com>
2016-11-16 01:45:20 -0300

    primer commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from paquetes.mayus import * # se llama solo minmay(let)
+#import paquetes.mayus # se llama mayus.minmay(let)
+
+def main():
+	print "Escriba una letra:"
+	let = raw_input()
+	print minmay(let)
+main()
---
+++ paquetes/mayus.py
@@ -0,0 +1,13 @@
+
+# -*- coding: utf-8 -*-
+def minmay(let):
+	mayusculas = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
+	minusculas = 'abcdefghijklmnopqrstuvwxyz'
+
+	if let in mayusculas :
+		return 'La letra es mayuscula'
+	elif let in minusculas :
+		return "La letra es minuscula"
+
+
+	
